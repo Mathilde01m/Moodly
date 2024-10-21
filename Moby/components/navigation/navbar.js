bc50dd191043a47a31011cebec4b66e81a95e32a
Mathilde01m <91540529+Mathilde01m@users.noreply.github.com>
2024-10-21 15:02:27 +0200

    update

--- Moby/components/navigation/navbar.js
+++ Moby/components/navigation/navbar.js
@@ -4,15 +4,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { Image } from 'react-native';
 
 // Importation des écrans
-import HomeScreen from '../../../screens/HomeScreen';
-import AssistanceScreen from '../../../screens/AssistanceScreen';
-import PayScreen from '../../../screens/PayScreen';
-import MailScreen from '../../../screens/MailScreen';
+import HomeScreen from '../../app/HomeScreen';
+import AssistanceScreen from '../../app/AssistanceScreen';
+import PayScreen from '../../app/PayScreen';
+import MailScreen from '../../app/MailScreen';
 
 // Créer le Tab Navigator
 const Tab = createBottomTabNavigator();
 
 export default function Navbar() {
+  console.log('Navbar is rendering')
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -30,6 +31,7 @@ export default function Navbar() {
             } else if (route.name === 'Mail') {
               iconName = require('../assets/images/mail.png');
             }
+            
 
             return <Image source={iconName} style={{ width: 25, height: 25, tintColor: focused ? 'black' : 'gray' }} />;
           },
@@ -38,11 +40,14 @@ export default function Navbar() {
           tabBarShowLabel: false, // Masquer les étiquettes sous les icônes
         })}
       >
+        
+        
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Assistance" component={AssistanceScreen} />
         <Tab.Screen name="Pay" component={PayScreen} />
         <Tab.Screen name="Mail" component={MailScreen} />
       </Tab.Navigator>
     </NavigationContainer>
+    
   );
 }
